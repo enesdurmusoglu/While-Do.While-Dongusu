f3efc0dbf201bd551cf3dcc510e63fc2228ca751
Enes Durmuşoğlu <73403774+enesdurmusoglu@users.noreply.github.com>
2022-11-10 22:07:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // while döngü örneği
+
+        int i = 1;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("While Döngüsü Bitti");
+
+
+        // Do-While döngüsü
+
+        int j = 1;
+        do {
+            System.out.println(j);
+            j++;
+        }while (j<10);
+
+        System.out.println("Do-While Döngüsü Bitti");
+    }
+}
+
+// While döngüsü ile Do-While döngüsü arasında ki fark.
+// Şart uymazsa While Döngüsü çalışmaz.
+// Şart uymazsa Do-While döngüsü en az bir kere çalışır.
+
+
